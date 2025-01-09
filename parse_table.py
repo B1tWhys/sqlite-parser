34c6bb2f5d88451969d518128af129f8119ccdc4
B1tWhys <skyarnold1@me.com>
2025-01-08 18:22:58 -0700

    I can read arbitrary rows! Huzzah!

--- parse_table.py
+++ parse_table.py
@@ -1,28 +1,13 @@
+import os
 import struct
-from abc import ABC
+from abc import ABC, abstractmethod
+from bisect import bisect_left
 from dataclasses import dataclass
 from enum import Enum
 from io import BytesIO
 from typing import Optional, List, BinaryIO
 
 import pytest
-
-
-def decode_varint(data, offset):
-    ans = 0
-    i = 0
-    while True:
-        val = data[offset + i]
-        if i < 8:
-            ans <<= 7
-            ans += val & ~0x80
-        else:
-            ans <<= 8
-            ans += val
-        i += 1
-        if i == 9 or not (val & 0x80):
-            break
-    return ans, i
 
 
 def read_varint(file):
@@ -105,7 +90,11 @@ class BTreePageType(Enum):
     INDEX_LEAF = 0x10
 
 
-class TableLeafCell:
+class Cell(ABC):
+    pass
+
+
+class TableLeafCell(Cell):
     row_id: int
     payload_size: int
     record: "Record"
@@ -114,6 +103,15 @@ class TableLeafCell:
         self.payload_size = read_varint(file)
         self.row_id = read_varint(file)
         self.record = Record(file)
+
+
+class TableInteriorCell(Cell):
+    child_page_ptr: int
+    key: int
+
+    def __init__(self, file: BinaryIO):
+        self.child_page_ptr = struct.unpack(">I", file.read(4))[0]
+        self.key = read_varint(file)
 
 
 class Record:
@@ -179,7 +177,7 @@ class PageHeader:
          self.cellContentAreaOffset,
          self.fragmentedFreeBytes) = struct.unpack(">3HB", file.read(7))
         if self.typeId in (BTreePageType.TABLE_INTERIOR, BTreePageType.INDEX_INTERIOR):
-            self.rightPtr = struct.unpack_from(">Q", file.read(4))[0]
+            self.rightPtr = struct.unpack(">I", file.read(4))[0]
             self.headerSize = 12
         else:
             self.headerSize = 8
@@ -188,57 +186,113 @@ class PageHeader:
 class Page(ABC):
     offset: int
     pageHeader: PageHeader
-    cells: List[TableLeafCell]
-    def __init__(self):
+    cells: List[Cell]
+
+    def __init__(self, file: BinaryIO):
+        self.offset = file.tell()
+        if self.offset <= 100:
+            self.offset = 0
         self.cells = []
+        self.pageHeader = PageHeader(file)
 
     @property
     def records(self):
         return [cell.record for cell in sorted(self.cells, key=lambda cell: cell.row_id)]
 
+    @staticmethod
+    def build_page(database: "Database", file: BinaryIO):
+        page_type = BTreePageType(file.read(1)[0])
+        file.seek(-1, os.SEEK_CUR)
+        match page_type:
+            case BTreePageType.TABLE_LEAF:
+                return BTreePage_TableLeaf(file)
+            case BTreePageType.TABLE_INTERIOR:
+                return BTreePage_TableInterior(file, database)
+            case _:
+                raise ValueError(f"Unsupported page type: {page_type}")
+
+    @abstractmethod
+    def get_record(self, key: int) -> Optional[Record]:
+        pass
+
 
 @dataclass
 class BTreePage_TableLeaf(Page):
-    def __init__(self, file: BinaryIO, page_includes_file_offset: bool = False):
-        super().__init__()
+    cells: List[TableLeafCell]
 
-        self.offset = file.tell()
-        if page_includes_file_offset:
-            self.offset -= 100
-        self.pageHeader = PageHeader(file)
+    def __init__(self, file: BinaryIO):
+        super().__init__(file)
+
         num_cells = self.pageHeader.numCellsInPage
         cell_ptrs = struct.unpack(f'>{num_cells}H', file.read(num_cells * 2))
         for ptr in cell_ptrs:
             file.seek(self.offset + ptr)
             self.cells.append(TableLeafCell(file))
 
+    def get_record(self, row_id: int) -> Optional[Record]:
+        idx = bisect_left(self.cells, row_id, key=lambda cell: cell.row_id)
+        if idx < len(self.cells) and self.cells[idx].row_id == row_id:
+            return self.cells[idx].record
+        else:
+            return None
+
+
+@dataclass
+class BTreePage_TableInterior(Page):
+    database: "Database"
+    cells: List[TableInteriorCell]
+
+    def __init__(self, file: BinaryIO, database: "Database"):
+        super().__init__(file)
+
+        self.database = database
+        num_cells = self.pageHeader.numCellsInPage
+        cell_ptrs = struct.unpack(f'>{num_cells}H', file.read(num_cells * 2))
+        for ptr in cell_ptrs:
+            file.seek(self.offset + ptr)
+            self.cells.append(TableInteriorCell(file))
+
+    def get_record(self, row_id) -> Optional[Record]:
+        idx = bisect_left(self.cells, row_id, key=lambda cell: cell.key)
+        if idx < len(self.cells) and self.cells[idx].key >= row_id:
+            child_page = self.database.get_page(self.cells[idx].child_page_ptr)
+        else:
+            child_page = self.database.get_page(self.pageHeader.rightPtr)
+        return child_page.get_record(row_id)
+
+
 class Database:
-    def __init__(self, file):
+    def __init__(self, file: BinaryIO):
         self.file = file
         self.header = FileHeader(file)
-        self.schema_page = BTreePage_TableLeaf(file, True)
+        self.schema_page = self.get_page(1)
+
+    def get_root_page_num(self, table_name):
+        for record in self.schema_page.records:
+            object_type, object_name, object_table, object_page, schema = record.values
+            if object_type == 'table' and object_name == table_name:
+                return object_page
+
+    def get_page(self, page_number) -> Page:
+        page_offset = (page_number - 1) * self.header.page_size
+        if page_offset == 0:
+            page_offset += 100
+        self.file.seek(page_offset)
+        return Page.build_page(self, self.file)
+
 
 def main():
-    fname = "./pages/001"
+    fname = "./example.db"
+    TARGET_ROW_ID = 413  # "450|user_450|user_450@example.com|password_450|2025-01-02 05:44:00"
     with open(fname, "rb") as file:
         db = Database(file)
-        for record in db.schema_page.records:
+        users_table_page_num = db.get_root_page_num("users")
+        users_table_root = db.get_page(users_table_page_num)
+        record = users_table_root.get_record(TARGET_ROW_ID)
+        if record is None:
+            print("Didn't find the record!")
+        else:
             print(record.values)
-
-
-@pytest.mark.parametrize(
-    "data, expected_val, expected_delta",
-    [
-        (b"\x00", 0, 1),
-        (b"\x81\x01", 0b10000001, 2),
-        (b"\x81\x81\x01", 0b100000010000001, 3),
-        (b"\x81" * 8 + b"\x01", 0x0204081020408101, 9),
-    ],
-)
-def test_decode_varint(data, expected_val, expected_delta):
-    actual_val, actual_delta = decode_varint(data, 0)
-    assert bin(actual_val) == bin(expected_val)
-    assert actual_delta == expected_delta
 
 
 @pytest.mark.parametrize(
